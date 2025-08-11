0b8ebf461907993ca16bf2bdcc028a6b63b0da0e
Kadri Kaljo <kadrikaljo@gmail.com>
2025-08-11 21:21:51 +0300

    added typography, grid system and icons

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,86 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
-    <div class="bg-white p-8 rounded-lg shadow-lg">
-      <h1 class="text-3xl font-bold text-gray-800 mb-4">Vite + Vue + Tailwind</h1>
-      <div class="flex space-x-4 mb-6">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-          <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
+  <div class="min-h-screen bg-nitro-grey-50 p-8">
+    <div class="max-w-4xl mx-auto">
+      <!-- Typography Test -->
+      <div class="mb-8">
+        <h1 class="text-display-1 font-display text-nitro-green-500 mb-4">Display 1 - Suur pealkiri</h1>
+        <h2 class="text-display-2 font-display text-nitro-green-600 mb-4">Display 2 - Väiksem pealkiri</h2>
+        
+        <h1 class="text-heading-1 font-body text-nitro-grey-800 mb-3">Heading 1 - Põhipealkiri</h1>
+        <h2 class="text-heading-2 font-body text-nitro-grey-700 mb-3">Heading 2 - Alampealkiri</h2>
+        <h3 class="text-heading-3 font-body text-nitro-grey-600 mb-3">Heading 3 - Keskmine pealkiri</h3>
+        <h4 class="text-heading-4 font-body text-nitro-grey-500 mb-3">Heading 4 - Väike pealkiri</h4>
+        <h5 class="text-heading-5 font-body text-nitro-grey-400 mb-3">Heading 5 - Väga väike pealkiri</h5>
+        <h6 class="text-heading-6 font-body text-nitro-grey-300 mb-3">Heading 6 - Kõige väiksem pealkiri</h6>
+        
+        <p class="text-body font-body text-nitro-grey-800 mb-2">Body text - Põhitekst 16px</p>
+        <p class="text-small font-body text-nitro-grey-600 mb-2">Small text - Väike tekst 14px</p>
+        <p class="text-tiny font-body text-nitro-grey-500">Tiny text - Väga väike tekst 12px</p>
       </div>
-      <HelloWorld msg="Vite + Vue" />
+
+      <!-- Grid System Test -->
+      <div class="mb-8">
+        <h2 class="text-heading-2 font-body text-nitro-green-600 mb-4">Grid System Test</h2>
+        
+        <!-- 12-kolonniline grid -->
+        <div class="grid grid-cols-12 gap-4 mb-6">
+          <div class="col-span-12 bg-nitro-green-100 p-4 rounded">
+            <p class="text-body font-body text-nitro-green-800">12 kolonni - täis laius</p>
+          </div>
+        </div>
+        
+        <div class="grid grid-cols-12 gap-4 mb-6">
+          <div class="col-span-6 bg-nitro-green-200 p-4 rounded">
+            <p class="text-body font-body text-nitro-green-800">6 kolonni - pool laiust</p>
+          </div>
+          <div class="col-span-6 bg-nitro-green-200 p-4 rounded">
+            <p class="text-body font-body text-nitro-green-800">6 kolonni - pool laiust</p>
+          </div>
+        </div>
+        
+        <div class="grid grid-cols-12 gap-4 mb-6">
+          <div class="col-span-4 bg-nitro-green-300 p-4 rounded">
+            <p class="text-body font-body text-nitro-green-800">4 kolonni</p>
+          </div>
+          <div class="col-span-4 bg-nitro-green-300 p-4 rounded">
+            <p class="text-body font-body text-nitro-green-800">4 kolonni</p>
+          </div>
+          <div class="col-span-4 bg-nitro-green-300 p-4 rounded">
+            <p class="text-body font-body text-nitro-green-800">4 kolonni</p>
+          </div>
+        </div>
+        
+        <div class="grid grid-cols-12 gap-4 mb-6">
+          <div class="col-span-3 bg-nitro-green-400 p-4 rounded">
+            <p class="text-small font-body text-nitro-green-800">3 kolonni</p>
+          </div>
+          <div class="col-span-3 bg-nitro-green-400 p-4 rounded">
+            <p class="text-small font-body text-nitro-green-800">3 kolonni</p>
+          </div>
+          <div class="col-span-3 bg-nitro-green-400 p-4 rounded">
+            <p class="text-small font-body text-nitro-green-800">3 kolonni</p>
+          </div>
+          <div class="col-span-3 bg-nitro-green-400 p-4 rounded">
+            <p class="text-small font-body text-nitro-green-800">3 kolonni</p>
+          </div>
+        </div>
+      </div>
+
+      <!-- Original content -->
+      <div class="bg-white p-8 rounded-lg shadow-lg">
+        <h1 class="text-heading-1 font-body text-nitro-green-500 mb-4">NITRO Design System</h1>
+        <div class="flex space-x-4 mb-6">
+          <a href="https://vite.dev" target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+          </a>
+          <a href="https://vuejs.org/" target="_blank">
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+          </a>
+        </div>
+        <HelloWorld msg="Typography System Working!" />
+      </div>
     </div>
   </div>
 </template>
