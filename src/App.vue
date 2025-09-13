b22831ef2ccc4188eb1444dc70f6fbdb23fb0a81
Marelle Palm <marelle.palm@gmail.com>
2025-09-13 13:44:13 +0300

    Service page part 1(ourServices)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Footer from './components/Footer.vue';
+import Service from './components/Service.vue';
 </script>
 <template>
   <section class="min-h-screen flex items-center justify-center bg-[#001412] text-white">
     <h1 class="text-4xl md:text-6xl font-semibold tracking-[-0.02em]">New beginning</h1>
   </section>
   <div>
+    <Service></Service>
     <Footer></Footer>
   </div>
 </template>
---
+++ src/components/Service.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Footer from './Footer.vue';
+import OurServices1 from './ServiceParts/OurServices1.vue';
+</script>
+
+<template>
+    <OurServices1></OurServices1>
+    <Footer></Footer>
+</template>
