1d3ad9fbecd08c45eae4dfae934d695f0eacc9c4
Marelle Palm <marelle.palm@gmail.com>
2025-09-15 23:13:52 +0300

    Service page part4(UxDesignProcess)

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -3,11 +3,13 @@ import Footer from './Footer.vue';
 import OurServices1 from './ServiceParts/OurServices1.vue';
 import OurCoreCapablities2 from './ServiceParts/OurCoreCapablities2.vue';
 import OurWorkProcess3 from './ServiceParts/OurWorkProcess3.vue';
+import UxDesignProcess4 from './ServiceParts/UxDesignProcess4.vue';
 </script>
 
 <template>
     <OurServices1></OurServices1>
     <OurCoreCapablities2></OurCoreCapablities2>
     <OurWorkProcess3></OurWorkProcess3>
+    <UxDesignProcess4></UxDesignProcess4>
     <Footer></Footer>
 </template>
