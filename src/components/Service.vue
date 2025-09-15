a09d186d1c6537d605e9b3964c2f04b76b4154cf
Marelle Palm <marelle.palm@gmail.com>
2025-09-15 22:31:42 +0300

    Service page part3(OurWorkProcess)

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -2,10 +2,12 @@
 import Footer from './Footer.vue';
 import OurServices1 from './ServiceParts/OurServices1.vue';
 import OurCoreCapablities2 from './ServiceParts/OurCoreCapablities2.vue';
+import OurWorkProcess3 from './ServiceParts/OurWorkProcess3.vue';
 </script>
 
 <template>
     <OurServices1></OurServices1>
     <OurCoreCapablities2></OurCoreCapablities2>
+    <OurWorkProcess3></OurWorkProcess3>
     <Footer></Footer>
 </template>
