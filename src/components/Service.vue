4246c3c77c46f9775a76646429ae2bf9c971ebf9
Marelle Palm <marelle.palm@gmail.com>
2025-09-15 21:25:32 +0300

    Service page part2(/OurCoreCapablities)

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
+<script setup lang="ts" >
 import Footer from './Footer.vue';
 import OurServices1 from './ServiceParts/OurServices1.vue';
+import OurCoreCapablities2 from './ServiceParts/OurCoreCapablities2.vue';
 </script>
 
 <template>
     <OurServices1></OurServices1>
+    <OurCoreCapablities2></OurCoreCapablities2>
     <Footer></Footer>
 </template>
---
+++ src/components/ServiceParts/OurCoreCapablities2.vue
@@ -0,0 +1,124 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="pt-[40px] pr-[140px] pb-[100px] pl-[140px] gap-[140px]">
+    <div class="">
+      <div>
+        <p class="text-[#33B8A9] md:text-[20px] text-center font-bold">
+          Our Core Capabilities
+        </p>
+        <p
+          class="text-[#004B49] font-semibold md:text-[64px] text-center font-semibold mb-8"
+        >
+          Our capabilities and expertise makes us stand out from the noise
+        </p>
+      </div>
+      <div class="flex flex-col gap-6 bg-white p-6">
+        <div class="flex gap-6">
+          <div class="flex-1 bg-teal-50 p-10 rounded-lg shadow">
+            <img
+              src="/home/marelle/nitro/my-vue-app-nitro/src/assets/Icons/ServiceIcons/UxIcon.png"
+              class="mb-5"
+              alt=""
+            />
+            <h3 class="text-[32px] font-semibold text-teal-700">
+              User Experience
+            </h3>
+            <ul class="mt-4 space-y-1 text-teal-700">
+              <li>Sitemaps</li>
+              <li>User Persona</li>
+              <li>User Journey</li>
+              <li>Wireframes</li>
+              <li>A/B Testing</li>
+              <li>Design Systems</li>
+              <li>Copy Writing / Editing & CTAs</li>
+            </ul>
+          </div>
+          <div class="flex-1 bg-teal-50 p-10 rounded-lg shadow">
+            <img
+              src="/home/marelle/nitro/my-vue-app-nitro/src/assets/Icons/ServiceIcons/DevelopmentIcons.png"
+              class="mb-5"
+              alt=""
+            />
+            <h3 class="text-[32px] font-semibold text-teal-700">Development</h3>
+            <ul class="mt-4 space-y-1 text-teal-700">
+              <li>Front-end Development</li>
+              <li>Back-end Development</li>
+              <li>Mobile App Development</li>
+              <li>CMS & CRM</li>
+              <li>Cyber Security</li>
+              <li>Support, QA & Maintenance</li>
+              <li>Search Engine Optimization (SEO)</li>
+            </ul>
+          </div>
+          <div class="flex-1 bg-teal-50 p-10 rounded-lg shadow">
+            <img
+              src="/home/marelle/nitro/my-vue-app-nitro/src/assets/Icons/ServiceIcons/TechnologyIcon.png"
+              class="mb-5"
+              alt=""
+            />
+            <h3 class="text-[32px] font-semibold text-teal-700">Technology</h3>
+            <ul class="mt-4 space-y-1 text-teal-700">
+              <li>Vue.js</li>
+              <li>Django</li>
+              <li>Python</li>
+              <li>React.js</li>
+              <li>Bootstrap</li>
+              <li>JavaScript</li>
+              <li>HTML, CSS</li>
+            </ul>
+          </div>
+        </div>
+        <div class="flex gap-6">
+          <div class="flex-1 bg-teal-50 p-10 rounded-lg shadow">
+            <img
+              src="/home/marelle/nitro/my-vue-app-nitro/src/assets/Icons/ServiceIcons/ResearchIcon.png"
+              class="mb-5"
+              alt=""
+            />
+            <h3 class="text-[32px] font-semibold text-teal-700">Research</h3>
+            <ul class="mt-4 space-y-1 text-teal-700">
+              <li>UX Audit</li>
+              <li>SWOT Analysis</li>
+              <li>User Research</li>
+              <li>Content Strategy</li>
+              <li>Product Strategy</li>
+              <li>Product Research & Analysis</li>
+            </ul>
+          </div>
+          <div class="flex-1 bg-teal-50 p-10 rounded-lg shadow">
+            <img
+              src="/home/marelle/nitro/my-vue-app-nitro/src/assets/Icons/ServiceIcons/VisualIcons.png"
+              class="mb-5"
+              alt=""
+            />
+            <h3 class="text-[32px] font-semibold text-teal-700">Visual</h3>
+            <ul class="mt-4 space-y-1 text-teal-700">
+              <li>User Interface (UI)</li>
+              <li>Website Interface</li>
+              <li>App Interface</li>
+              <li>Dashboard</li>
+              <li>Design Systems</li>
+              <li>Brand Systems</li>
+            </ul>
+          </div>
+          <div class="flex-1 bg-teal-50 p-10 rounded-lg shadow">
+            <img
+              src="/home/marelle/nitro/my-vue-app-nitro/src/assets/Icons/ServiceIcons/InteractionsIcons.png"
+              class="mb-5"
+              alt=""
+            />
+            <h3 class="text-[32px] font-semibold text-teal-700">
+              Interactions
+            </h3>
+            <ul class="mt-4 space-y-1 text-teal-700">
+              <li>User Flow</li>
+              <li>Clickable Prototypes</li>
+              <li>UI Animations</li>
+              <li>Smooth Intuitive Interactions</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
