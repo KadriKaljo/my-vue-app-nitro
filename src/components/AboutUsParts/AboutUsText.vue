b54247effdf02cee747d91630993a8d572fef411
Marelle Palm <marelle.palm@gmail.com>
2025-09-19 22:33:20 +0300

    AboutUs

---
+++ src/components/AboutUsParts/AboutUsText.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="bg-[#E5F6F4] px-[100px] py-[140px] gap-10 flex flex-col">
+    <div class="max-w-[800px]">
+        <p class="text-[72px] font-semibold">Designers, Developers </br>and Homo Sapiens.</p>
+    </div>
+    <div class="flex justify-end ">
+        <p class="text-[#008576] text-[48px] max-w-[700px] ">
+        We are a small team but a force of nature. We are determined, we work
+        non-stop and build relationships that our clients return to. We enjoy
+        making our client’s businesses a success and go the extra mile to
+        provide a service that creates a memorable experience.
+        </p>
+    </div>
+  </div>
+</template>
---
+++ src/components/AboutUsParts/Team.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="px-[100px] py-[140px] gap-4 bg-white">
+        <div>
+            <p class="font-semibold text-[64px] text-[#004849] mb-10">Meet the Team</p>
+            <div class="flex gap-20 mb-15">
+                <div>
+                    <div class="relative group mb-10">
+                        <img src="/home/marelle/nitro/my-vue-app-nitro/src/assets/Icons/ServiceIcons/CeoImage.png" class="h-[530px] w-[525px]" alt="">
+                        <div class="absolute inset-0 flex items-center justify-center bg-[#09ADA8E5] opacity-0 group-hover:opacity-100 transition">
+                            <div class="flex gap-3 mb-5">
+                                <a href="#" class="hover:opacity-75 bg-white rounded"><i class="fab fa-facebook fa-2x"></i></a>
+                                <a href="#" class="hover:opacity-75  bg-white rounded"><i class="fab fa-twitter fa-2x"></i></a>
+                                <a href="#" class="hover:opacity-75  bg-white rounded"><i class="fab fa-instagram fa-2x"></i></a> 
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-[32px] mb-10">Yogarasa Gandhi</p>
+                        <p class="text-[20px]">CEO & Director</p>
+                    </div>
+                </div>
+                <div>
+                    <div class="relative group mb-10">
+                        <img src="/home/marelle/nitro/my-vue-app-nitro/src/assets/Icons/ServiceIcons/CeoImage.png" class="h-[530px] w-[525px]" alt="">
+                        <div class="absolute inset-0 flex items-center justify-center bg-[#09ADA8E5] opacity-0 group-hover:opacity-100 transition">
+                            <div class="flex gap-3 mb-5">
+                                <a href="#" class="hover:opacity-75 bg-white rounded"><i class="fab fa-facebook fa-2x"></i></a>
+                                <a href="#" class="hover:opacity-75  bg-white rounded"><i class="fab fa-twitter fa-2x"></i></a>
+                                <a href="#" class="hover:opacity-75  bg-white rounded"><i class="fab fa-instagram fa-2x"></i></a> 
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-[32px] mb-10 ">Nnenna Jioke</p>
+                        <p class="text-[20px]">COO & Product Manager</p>
+                    </div>
+                </div>
+                <div>
+                    <div class="relative group mb-10">
+                        <img src="/home/marelle/nitro/my-vue-app-nitro/src/assets/Icons/ServiceIcons/CeoImage.png" class="h-[530px] w-[525px]" alt="">
+                        <div class="absolute inset-0 flex items-center justify-center bg-[#09ADA8E5] opacity-0 group-hover:opacity-100 transition">
+                            <div class="flex gap-3 mb-5">
+                                <a href="#" class="hover:opacity-75 bg-white rounded"><i class="fab fa-facebook fa-2x"></i></a>
+                                <a href="#" class="hover:opacity-75  bg-white rounded"><i class="fab fa-twitter fa-2x"></i></a>
+                                <a href="#" class="hover:opacity-75  bg-white rounded"><i class="fab fa-instagram fa-2x"></i></a> 
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-[32px] mb-10 ">Yogarasa Gandhi</p>
+                        <p class="text-[20px]">Motion Designer</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex gap-20 mb-15">
+                <div>
+                    <div class="relative group mb-10">
+                        <img src="/home/marelle/nitro/my-vue-app-nitro/src/assets/Icons/ServiceIcons/CeoImage.png" class="h-[530px] w-[525px]" alt="">
+                        <div class="absolute inset-0 flex items-center justify-center bg-[#09ADA8E5] opacity-0 group-hover:opacity-100 transition">
+                            <div class="flex gap-3 mb-5">
+                                <a href="#" class="hover:opacity-75 bg-white rounded"><i class="fab fa-facebook fa-2x"></i></a>
+                                <a href="#" class="hover:opacity-75  bg-white rounded"><i class="fab fa-twitter fa-2x"></i></a>
+                                <a href="#" class="hover:opacity-75  bg-white rounded"><i class="fab fa-instagram fa-2x"></i></a> 
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-[32px] mb-10">Yogarasa Gandhi</p>
+                        <p class="text-[20px]">Digital Marketer</p>
+                    </div>
+                </div>
+                <div>
+                    <div class="relative group mb-10">
+                        <img src="/home/marelle/nitro/my-vue-app-nitro/src/assets/Icons/ServiceIcons/CeoImage.png" class="h-[530px] w-[525px]" alt="">
+                        <div class="absolute inset-0 flex items-center justify-center bg-[#09ADA8E5] opacity-0 group-hover:opacity-100 transition">
+                            <div class="flex gap-3 mb-5">
+                                <a href="#" class="hover:opacity-75 bg-white rounded"><i class="fab fa-facebook fa-2x"></i></a>
+                                <a href="#" class="hover:opacity-75  bg-white rounded"><i class="fab fa-twitter fa-2x"></i></a>
+                                <a href="#" class="hover:opacity-75  bg-white rounded"><i class="fab fa-instagram fa-2x"></i></a> 
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-[32px] mb-10 ">Nnenna Jioke</p>
+                        <p class="text-[20px]">Product Designer</p>
+                    </div>
+                </div>
+                <div>
+                    <div class="relative group mb-10">
+                        <img src="/home/marelle/nitro/my-vue-app-nitro/src/assets/Icons/ServiceIcons/CeoImage.png" class="h-[530px] w-[525px]" alt="">
+                        <div class="absolute inset-0 flex items-center justify-center bg-[#09ADA8E5] opacity-0 group-hover:opacity-100 transition">
+                            <div class="flex gap-3 mb-5">
+                                <a href="#" class="hover:opacity-75 bg-white rounded"><i class="fab fa-facebook fa-2x"></i></a>
+                                <a href="#" class="hover:opacity-75  bg-white rounded"><i class="fab fa-twitter fa-2x"></i></a>
+                                <a href="#" class="hover:opacity-75  bg-white rounded"><i class="fab fa-instagram fa-2x"></i></a> 
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-[32px] mb-10 ">Yogarasa Gandhi</p>
+                        <p class="text-[20px]">Web Developer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
